d12b9621de039945f1368774d908ed2621d19e87
Bochkov1 <116495826+Bochkov1@users.noreply.github.com>
2025-09-20 00:30:12 +0200

    Add files via upload

--- minitorch/operators.py
+++ minitorch/operators.py
@@ -1,9 +1,10 @@
 """Collection of the core mathematical operators used throughout the code base."""
 
 import math
+from typing import Callable, Iterable
 
 # ## Task 0.1
-from typing import Callable, Iterable, List
+
 
 #
 # Implementation of a prelude of elementary functions.
@@ -32,20 +33,24 @@ from typing import Callable, Iterable, List
 # $f(x) = |x - y| < 1e-2$
 
 
-
 # TODO: Implement for Task 0.1.
+
 
 def mul(x: float, y: float) -> float:
     return x * y
 
+
 def id(x: float) -> float:
     return x
+
 
 def add(x: float, y: float) -> float:
     return x + y
 
+
 def neg(x: float) -> float:
     return -x
+
 
 def lt(x: float, y: float) -> float:
     if x < y:
@@ -60,14 +65,17 @@ def eq(x: float, y: float) -> float:
     else:
         return 0.0
 
+
 def max(x: float, y: float) -> float:
     if x > y:
         return x
     else:
         return y
 
+
 def is_close(x: float, y: float) -> bool:
     return abs(x - y) < 1e-2
+
 
 def sigmoid(x: float) -> float:
     if x >= 0.0:
@@ -77,17 +85,21 @@ def sigmoid(x: float) -> float:
         z = math.exp(x)
         return z / (1.0 + z)
 
+
 def relu(x: float) -> float:
     if x > 0.0:
         return x
     else:
         return 0.0
 
+
 def log(x: float) -> float:
     return math.log(x)
 
+
 def exp(x: float) -> float:
     return math.exp(x)
+
 
 def log_back(y: float, x: float) -> float:
     if x == 0.0:
@@ -95,8 +107,10 @@ def log_back(y: float, x: float) -> float:
     else:
         return y / x
 
+
 def inv(x: float) -> float:
     return 1.0 / x
+
 
 def inv_back(y: float, x: float) -> float:
     if x == 0.0:
@@ -107,11 +121,13 @@ def inv_back(y: float, x: float) -> float:
             return 0.0
         return -y / x_squared
 
+
 def relu_back(x: float, y: float) -> float:
     if x > 0.0:
         return y
     else:
         return 0.0
+
 
 # ## Task 0.3
 
@@ -131,31 +147,40 @@ def relu_back(x: float, y: float) -> float:
 
 # TODO: Implement for Task 0.3.
 
+
 def map(f: Callable[[float], float], x: Iterable[float]) -> list[float]:
     result = []
     for i in x:
         result.append(f(i))
     return result
 
-def zipWith(f: Callable[[float, float], float], a: Iterable[float], b: Iterable[float]) -> list[float]:
+
+def zipWith(
+    f: Callable[[float, float], float], a: Iterable[float], b: Iterable[float]
+) -> list[float]:
     result = []
     for i, j in zip(a, b):
         result.append(f(i, j))
     return result
+
 
 def reduce(f: Callable[[float, float], float], x: Iterable[float], s: float) -> float:
     for i in x:
         s = f(s, i)
     return s
 
+
 def negList(x: Iterable[float]) -> list[float]:
     return map(neg, x)
+
 
 def addLists(a: Iterable[float], b: Iterable[float]) -> list[float]:
     return zipWith(add, a, b)
 
+
 def sum(x: Iterable[float]) -> float:
     return reduce(add, x, 0.0)
+
 
 def prod(x: Iterable[float]) -> float:
     return reduce(mul, x, 1.0)
